4e8b3b7b571413d91d3e633ba7c42cc567536e70
bcwan <bcwan8@gmail.com>
2020-08-13 21:24:14 -0700

    Create block structure

--- block.js
+++ block.js
@@ -1,8 +1,18 @@
 class Block {
-  constructor(timestamp, lastHash, hash, data) {
+  // wrapping parameters into objects allows us to put input arguments in any order upon creation
+  constructor({ timestamp, lastHash, hash, data }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
   }
 }
+
+const block1 = new Block({
+  timestamp: '01/01/01', 
+  lastHash: 'foo-lastHash', 
+  hash: 'foo-hash', 
+  data: 'foo-data' 
+});
+
+console.log('block1', block1)
