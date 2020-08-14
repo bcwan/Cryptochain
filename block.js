1dd49fc0db2ae753a7455551da14b1a70e2958e5
bcwan <bcwan8@gmail.com>
2020-08-13 22:56:13 -0700

    Add hashing to each block mining

--- block.js
+++ block.js
@@ -1,4 +1,5 @@
 const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
 
 class Block {
   // wrapping parameters into objects allows us to put input arguments in any order upon creation
@@ -15,10 +16,14 @@ class Block {
   }
 
   static mineBlock( { lastBlock, data }) {
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+
     return new this({
-      timestamp: Date.now(),
-      lastHash: lastBlock.hash,
-      data
+      timestamp,
+      lastHash,
+      data,
+      hash: cryptoHash(timestamp, lastHash, data)
     });
   }
 }
