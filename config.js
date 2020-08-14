72239273875b992ad8480a188e62d05f2b88c2eb
bcwan <bcwan8@gmail.com>
2020-08-13 21:42:36 -0700

    Create config file to store global variables and genesis block info

---
+++ config.js
@@ -0,0 +1,9 @@
+// hard coded data for genesis or global values
+const GENESIS_DATA = { 
+  timestamp: 1,
+  lastHash: '-----',
+  hash: 'hash-one',
+  data: []
+}
+
+module.exports = { GENESIS_DATA };
