f35ac928cfc8c7ea5750799fdfb6fcc0b2314b93
bcwan <bcwan8@gmail.com>
2020-08-14 22:04:25 -0700

    Check to see if genesis block matches with that of the chain and compares block hashes

--- blockchain.js
+++ blockchain.js
@@ -5,6 +5,7 @@ class Blockchain {
     this.chain = [Block.genesis()];
   }
 
+  // add blocks after creation of genesis block, appends after previous block
   addBlock({ data }) {
     const newBlock = Block.mineBlock({
       lastBlock: this.chain[this.chain.length - 1],
@@ -12,6 +13,20 @@ class Blockchain {
     });
 
     this.chain.push(newBlock);
+  }
+
+  // checks to see if chain doesn't have discrepancies
+  static isValidChain(chain) {
+    // use stringify to convert objects into string values, and compare them if they are the same values
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+
+    // check to see if the lastHash is consistant to previous blocks hash
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+      const actualLastHash = chain[i - 1].hash;
+      const { timestamp, lastHash, hash, data } = block;
+      if (lastHash !== actualLastHash) return false;
+    }
   }
 }
 
