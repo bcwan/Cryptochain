239d4d4b8283f6c8b06093758aa312e5d86175e3
bcwan <bcwan8@gmail.com>
2020-08-16 00:12:32 -0700

    Increase or decrease difficulty in finding hash nonce when adding blocks

--- block.js
+++ block.js
@@ -1,4 +1,4 @@
-const { GENESIS_DATA } = require('./config');
+const { GENESIS_DATA, MINE_RATE } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
 class Block {
@@ -41,6 +41,17 @@ class Block {
       nonce,
       hash: cryptoHash(timestamp, lastHash, data, nonce, difficulty)
     });
+  }
+
+  // adjust difficulty based on how long it takes to make a block
+  static adjustDifficulty({ originalBlock, timestamp }) {
+    const { difficulty } = originalBlock;
+    const difference = timestamp - originalBlock.timestamp;
+    if (difference > MINE_RATE) {
+      return difficulty - 1;
+    } else if (difference < MINE_RATE) {
+      return difficulty + 1;
+    }
   }
 }
 
