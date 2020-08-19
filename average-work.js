f0cab3d990b4c68739c2612ed0d04419caa9c8ef
bcwan <bcwan8@gmail.com>
2020-08-18 23:18:39 -0700

    Add secondary block to that of genesis block

--- average-work.js
+++ average-work.js
@@ -2,6 +2,10 @@ const Blockchain = require('./blockchain');
 
 const blockchain = new Blockchain();
 
+blockchain.addBlock({ data: 'initial '})
+
+console.log('first block', blockchain.chain[blockchain.chain.length - 1]);
+
 // timestamps help us generate hashes
 let prevTimestamp;
 let nextTimestamp;
@@ -28,5 +32,5 @@ for (let i = 0; i < 10000; i++) {
 
   average = times.reduce((total, num) => (total + num)) / times.length;
 
-  console.log(`Time to mine block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`)
+  //console.log(`Time to mine block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`)
 }
