ef02e3edce3bb91336bd1211896879aa17489a06
bcwan <bcwan8@gmail.com>
2020-08-18 23:11:01 -0700

    Convert hex hash to binary hash for harder PoW

--- crypto-hash.js
+++ crypto-hash.js
@@ -1,5 +1,6 @@
 // npm crypto library
 const crypto = require('crypto');
+const hextToBinary = require('hex-to-binary');
 
 const cryptoHash = (...inputs) => {
   // inport SHA-256 algorithm
@@ -9,8 +10,8 @@ const cryptoHash = (...inputs) => {
   // sort to make input values ordered to generate uniform hash
   hash.update(inputs.sort().join(' '));
 
-  // return digest or result of array in hex form
-  return hash.digest('hex');
+  // return digest or result of array in hex form, then hextToBinary will convert the hex to binary
+  return hexToBinary(hash.digest('hex'));
 };
 
 module.exports = cryptoHash;
